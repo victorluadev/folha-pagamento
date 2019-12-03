b75ca8337333f866f4e80ab74941d393a825454c
Matheus <matheusgt4@hotmail.com>
2019-12-03 07:33:42 -0300

    Novos calculos e melhoria na apresentação dos valores

--- js/init.js
+++ js/init.js
@@ -24,13 +24,16 @@ jQuery(document).ready(function ($) {
         "dep": dep
       };
 
-    let x = localStorage.length + 1;
+    let x = localStorage.length;
     localStorage[x] = JSON.stringify(vamos);
   }
 
+  function excluirFuncionario(f){
+    localStorage.removeItem(f);
+  }
+  
   // Tentando pegar dados passados
   try{
-
     if( (localStorage[0] == null) && (localStorage[1] == null) ) {
       comecarFuncionario();
     }
@@ -42,18 +45,20 @@ jQuery(document).ready(function ($) {
   
   // Gerando dados padrão
   function comecarFuncionario(){
-    funcionarios ={ 
+    funcionarios = { 
         "nome": "Ricardo",
         "bruto": "3500",
         "dep": "2"
       };
     localStorage.setItem(0, JSON.stringify(funcionarios));
+
     funcionarios = {
         "nome": "Sabrina",
         "bruto": "9000",
         "dep": "1"
       };
     localStorage.setItem(1, JSON.stringify(funcionarios));
+
     console.log("Salvando funcionarios padrão no LocalStorage");
   }
 
@@ -67,16 +72,22 @@ jQuery(document).ready(function ($) {
 
   // Apenas imprimir os funcionarios na tabela
   function mostrarFuncionario(f) {
-    inss = calculoINSS(f.bruto);
-    irrf = calculoIRRF(f.bruto, inss);
-    liquido = f.bruto - inss - irrf;
+    bruto = accounting.formatMoney(f.bruto, "R$", 2, ".", ",");
+    inss = accounting.formatMoney(calculoINSS(f.bruto), "R$", 2, ".", ",");
+    irrf = accounting.formatMoney(calculoIRRF(f.bruto, inss), "R$", 2, ".", ",");
+    liquido = (f.bruto - calculoINSS(f.bruto) - calculoIRRF(f.bruto, inss));
+    inssPatri = accounting.formatMoney(calculoInssPatri(f.bruto), "R$", 2, ".", ",");
+    fgts = accounting.formatMoney(calculoFGTS(f.bruto), "R$", 2, ".", ",");
+    guiaInss = accounting.formatMoney(calculoGuiaInss(f.bruto), "R$", 2, ".", ",");
 
     html = "<tr>";
     html += "<td>" + f.nome + "</td>";
-    html += "<td>" + f.bruto + "</td>";
+    html += "<td>" + bruto + "</td>";
     html += "<td>" + f.dep + "</td>";
     html += "<td>" + inss + "</td>";
     html += "<td>" + irrf + "</td>";
+    html += "<td>" + inssPatri + " (" + guiaInss + ")</td>";
+    html += "<td>" + fgts + "</td>";
     html += "<td>" + liquido + "</td>";
     html += "</tr>";
 
@@ -84,6 +95,15 @@ jQuery(document).ready(function ($) {
   };
 
   // Calculo feito a parti da tabela do contribuinte
+  function calculoFGTS(valor){
+    return valor * 0.08;
+  }
+  function calculoGuiaInss(valor){
+    base = calculoINSS(valor) + calculoInssPatri(valor);
+    rat = (calculoINSS(valor) + calculoInssPatri(valor)) * 0.2;
+    outros = (calculoINSS(valor) + calculoInssPatri(valor)) * 0.058;
+    return base + rat + outros;
+  }
   function calculoINSS(n) {
     if (n <= 1399.12) {
       return n * 0.08;
@@ -94,6 +114,19 @@ jQuery(document).ready(function ($) {
     if ((n >= 2331.88) && (n <= 4663.75)) {
       return n * 0.11;
     }
+
+    return n * 0.11;
+  }
+
+  function calculoInssPatri(valor){
+    if (valor <= 678){
+      return valor * 0.11;
+    }
+    if ((valor >= 678) && (valor <= 4159)){
+      return valor * 0.20;
+    }
+
+    return valor * 0.20;
   }
 
   function calculoIRRF(salario, inss) {
@@ -117,5 +150,7 @@ jQuery(document).ready(function ($) {
     if ((valor >= 4664.68) && (valor <= 2826.65)) {
       return valor * 0.2750 - 869.36;
     }
+
+    return valor * 0.2750 - 869.36;
   }
 });
